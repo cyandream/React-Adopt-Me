f22e5178a558e6e05cd4d3526d095691f8189bf0
Michelle Burton <michelle_burton@bradycorp.com>
2019-09-27 10:36:25 -0500

    Added API Call, live data with useEffect hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
 
   const clickHandler = e => {
     e.preventDefault;
     setLocation(e.target.value);
   };
+
+  // Scheduling when this function happens
+  // 1st Render, then soon after this function runs
+  // Why? - so the API call does not slow down the first page render
+  useEffect(() => {
+    // pet.breeds('dog').then(console.log, console.error);
+
+    // OnSelect  update the breeds dropdown
+    setBreeds([]);
+
+    // Clear out dropdown, before repopulating
+    setBreed('');
+
+    // Call API to populate, based on animal, return promise
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      // transform list of objects into a list of strings ==map
+
+      // no destructuring
+      //const breedStrings = breeds.map((obj.name ) => obj.name);
+
+      // distructrure object into name
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+
+    // Run only when things change,  list of depenency
+  }, [animal, setBreeds, setBreed]);
 
   return (
     <div className="search-params">
